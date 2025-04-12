f1d955d096589192be56ce8d864898574e70cd3e
Yojjal Sharma <75210990+yojjal060@users.noreply.github.com>
2025-04-12 12:26:21 +0545

    fix title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Yojjal Sharma - Portfolio',
+  description: 'Created with Passion',
+  generator: 'heart',
 }
 
 export default function RootLayout({
